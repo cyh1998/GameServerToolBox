e265e6fa7ac45712cceea6288b8ce3f058823cb3
cyh <971415706@qq.com>
2023-01-19 14:39:33 +0800

    remove warning

--- utility/func/FuncStr.cpp
+++ utility/func/FuncStr.cpp
@@ -8,8 +8,8 @@ extern std::string trim_str(const std::string & str)
 		dest.erase(0, dest.find_first_not_of('\0'));
 		dest.erase(dest.find_first_not_of('\0') + 1);
 
-		dest.erase(0, dest.find_first_not_of(' \n\r\t'));
-		dest.erase(dest.find_first_not_of(' \n\r\t') + 1);
+		dest.erase(0, dest.find_first_not_of(" \n\r\t"));
+		dest.erase(dest.find_first_not_of(" \n\r\t") + 1);
 	}
 	return dest;
 }
